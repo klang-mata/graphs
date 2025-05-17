ea7250a59622768922fc28ad9b15ae46deec429e
Matej Klang <klangm@jirovcovka.net>
2025-05-17 23:23:22 +0200

    Final version (v1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="graphs",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     description="A library for graph operations",
     author="Matej Klang",
--- testing/test.py
+++ testing/test.py
@@ -1,21 +1,26 @@
 from graphs import (vertex, edge, graph, graph_tools)
 
 def main():
-    graph1 = graph.UnorderedGraph()
-    node0 = vertex.Vertex(graph1)
-    node1 = vertex.Vertex(graph1)
-    node2 = vertex.Vertex(graph1)
-    node3 = vertex.Vertex(graph1)
-    node4 = vertex.Vertex(graph1)
-    node5 = vertex.Vertex(graph1)
+    graph1 = graph.DirectedGraph()
+    node0 = vertex.Vertex()
+    node1 = vertex.Vertex()
+    node2 = vertex.Vertex()
+    node3 = vertex.Vertex()
+    node4 = vertex.Vertex()
+    node5 = vertex.Vertex()
+
+    node0.add_to_graph(graph1)
+    node1.add_to_graph(graph1)
+    node2.add_to_graph(graph1)
+    node3.add_to_graph(graph1)
+    node4.add_to_graph(graph1)
+    node5.add_to_graph(graph1)
 
     node0.add_neighbor(1)
-    node0.add_neighbor(3)
     node1.add_neighbor(2)
     node2.add_neighbor(3)
     node3.add_neighbor(4)
-    node4.add_neighbor(0)
-    node5.add_neighbor(0)
+    node0.add_neighbor(4)
 
     dfsresult = graph_tools.GraphTools.depth_first(graph1, 0, 4)
     print(dfsresult)
@@ -23,20 +28,11 @@ def main():
     bfsresult = graph_tools.GraphTools.breadth_first(graph1, 0)
     print(bfsresult)
 
-    node0.remove_neighbor(1)
-    node0.print_neighbors()
+    dijkstraresult = graph_tools.GraphTools.dijkstra(graph1, 0, 2)
+    print(dijkstraresult)
 
-    dijkstraresult = graph_tools.GraphTools.dijkstra(graph1, 0, 4)
-
-#if __name__ == "test":
+    cycleresult = graph_tools.GraphTools.detect_cycle(graph1)
+    print(cycleresult)
+    
 main()
 
-#TODO:
-#   use vertex or vertex_key for adding edges? (probably vertex_key but its harder to implement) - done
-#   depth first search - done
-#   breadth first search - done
-#   dijkstra
-#   detect cycles
-#   make a Graph.grid(size_a, size_b) method for demonstrating pathfinding?
-#   simple UI for visual demonstration
-#   error handling, optimization, QOL and utility
